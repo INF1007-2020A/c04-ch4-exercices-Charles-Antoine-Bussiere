d3c8ae05cb83b0d4f44cd6c707c9d8668aeb17bb
Charles-Antoine-Bussiere <charles-antoine.bussiere@polymtl.ca>
2020-09-15 23:43:48 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -20,7 +20,7 @@ def replace_char(string: str, old_char: str, new_char: str) -> str:
     for i in range(len(string)):
         if string[i] is old_char:
             string = string[ :i] + new_char + string[i+1:]
-    return a
+    return string
 
 
 def get_number_of_char(string: str, char: str) -> int:
