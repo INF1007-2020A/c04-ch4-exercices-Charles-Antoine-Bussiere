c84d0cd594d75ae4df97f26bfc389598b50cec17
Charles-Antoine-Bussiere <charles-antoine.bussiere@polymtl.ca>
2020-09-21 22:25:30 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -33,7 +33,8 @@ def get_number_of_char(string: str, char: str) -> int:
 
 def get_number_of_words(sentence: str, word: str) -> int:
     x = 0
-    for i in 
+    for i in sentence:
+        if i is ' ':
 
 
 def main() -> None:
